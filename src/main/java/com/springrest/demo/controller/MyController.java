b3d6e5d9fa8b205b74dfec53d5cbd7f856350b8a
Priyanshu Sharan <priyanshu.sharan@credextechnology.com>
2023-06-21 12:08:18 +0530

    changes for db connection

--- src/main/java/com/springrest/demo/controller/MyController.java
+++ src/main/java/com/springrest/demo/controller/MyController.java
@@ -18,59 +18,57 @@ import com.springrest.demo.util.DBUtility;
 
 @RestController
 public class MyController {
-	
-    @GetMapping("products")
-    public List<Product> getAllProducts() {
-    	EntityManager entityManager = DBUtility.getEntityManager();
-        TypedQuery<Product> query = entityManager.createQuery("SELECT o FROM " + Product.class.getSimpleName() + " o",
-                Product.class);
-        return query.getResultList();
-    }
-    
+
+	@GetMapping("/products")
+	public List<Product> getAllProducts() {
+		EntityManager entityManager = DBUtility.getEntityManager();
+		TypedQuery<Product> query = entityManager.createQuery("SELECT o FROM " + Product.class.getSimpleName() + " o",
+				Product.class);
+		return query.getResultList();
+	}
+
 //    String sql = "SELECT level2 FROM category WHERE level1 = 'Clothing' AND level2 IS NOT NULL";
 
-      @GetMapping("/products/{level1}/{level2}")
-      public Product getProductByTitleAndCategoryId(
-    		  @PathVariable("level1") String level1,
-    	      @PathVariable("level2") String level2) {
-    	  
-        EntityManager entityManager = DBUtility.getEntityManager();
-        String query1 = "SELECT * FROM product " +
-                "JOIN category ON product.category_id = category.id " +
-                "WHERE level1 = :level1 AND level2 = :level2;";
-        TypedQuery<Product> typedQuery = entityManager.createQuery(query1, Product.class);
-        typedQuery.setParameter("level1", level1);
-        typedQuery.setParameter("level2", level2);
-        return typedQuery.getSingleResult();
-    }
-      
-    @GetMapping("/products/{productId}")
-    public Product getProductById(@PathVariable Long id) {
-    	EntityManager entityManager= DBUtility.getEntityManager();
-        return entityManager.find(Product.class, id);
-    }
+	@GetMapping("/products/{level1}/{level2}")
+	public Product getProductByTitleAndCategoryId(@PathVariable("level1") String level1,
+			@PathVariable("level2") String level2) {
+
+		EntityManager entityManager = DBUtility.getEntityManager();
+		String query1 = "SELECT * FROM product " + "JOIN category ON product.category_id = category.id "
+				+ "WHERE level1 = :level1 AND level2 = :level2;";
+		TypedQuery<Product> typedQuery = entityManager.createQuery(query1, Product.class);
+		typedQuery.setParameter("level1", level1);
+		typedQuery.setParameter("level2", level2);
+		return typedQuery.getSingleResult();
+	}
+
+	@GetMapping("/products/{productId}")
+	public Product getProductById(@PathVariable Long id) {
+		EntityManager entityManager = DBUtility.getEntityManager();
+		return entityManager.find(Product.class, id);
+	}
 
 	@PostMapping("/products")
 	public Product addProduct(@RequestBody Product product) {
-	    EntityManager entityManager = null;
-	    EntityTransaction transaction = null;
-	    try {
-	        entityManager = DBUtility.getEntityManager();
-	        transaction = entityManager.getTransaction();
-	        transaction.begin();
-	        entityManager.persist(product);
-	        transaction.commit();
-	    } catch (RollbackException e) {
-	    	if (transaction != null && transaction.isActive()) {
-	            transaction.rollback();
-	        }
-	        e.printStackTrace();
-	    } finally {
-	        if (entityManager != null && entityManager.isOpen()) {
-	            entityManager.close();
-	        }
-	    }
-	    return product;
+		EntityManager entityManager = null;
+		EntityTransaction transaction = null;
+		try {
+			entityManager = DBUtility.getEntityManager();
+			transaction = entityManager.getTransaction();
+			transaction.begin();
+			entityManager.persist(product);
+			transaction.commit();
+		} catch (RollbackException e) {
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (entityManager != null && entityManager.isOpen()) {
+				entityManager.close();
+			}
+		}
+		return product;
 	}
-	
+
 }
--- src/main/java/com/springrest/demo/entities/ProductAttribute.java
+++ src/main/java/com/springrest/demo/entities/ProductAttribute.java
@@ -4,68 +4,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
 @Table(name = "attribute")
-public class ProductAttribute {
-	 @Id
-	 @GeneratedValue(strategy = GenerationType.IDENTITY)
-	 private Long attributeid;
-	 
-	 @ManyToOne
-	 private Product product;
+public @Data class ProductAttribute {
 
-	 private String color;
-	 private String size;
-	 
-	 public ProductAttribute(String size, String color) {
-	        super();
-	        this.setSize(size);
-	        this.setColor(color);
-	    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long attributeid;
 
-	
+	private Long productid;
 
-	public ProductAttribute() {
-		 super();
-	 }
-	 
-	 public Long getAttributeid() {
-		return attributeid;
-	}
+	private String color;
 
-	public void setAttributeid(Long attributeid) {
-		this.attributeid = attributeid;
-	}
-	
-	public String getColor() {
-		return color;
-	}
+	private String size;
 
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public String getSize() {
-		return size;
-	}
-
-	public void setSize(String size) {
-		this.size = size;
-	}
-	
-	 @Override
-	 public String toString() {
-		return "ProductAttribute [attributeid=" + attributeid + ", color=" + color + ", size=" + size + "]";
-		}
-	 
-	 public void setProduct(Product product) {
-	        this.product = product;
-	    }
-
-	    public Product getProduct() {
-	        return product;
-	    }
 }
